c04f546598682e45ad1aed1be37eae9e131cabf0
kmarella <Kishore.Marella@valic.com>
2019-02-20 13:48:51 -0600

    Grid in overview

--- src/Overview/index.jsx
+++ src/Overview/index.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React from 'react'
+import LineChart from '../LineChart';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LineChart from '../LineChart';
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 6,
+    textAlign: 'center',
+    borderRadius: 12,
+    height: 140,
+    margin: 10,
+    color: theme.palette.text.secondary,
+  },
+});
 
-function CenteredGrid() {
+function CenteredGrid(props) {
+  const { classes } = props;
+
   return (
     <Grid item xs={12}>
       <Paper>
         <LineChart />
       </Paper>
+      <Grid container>
+        <Grid item xs={6} sm={3}>
+          <Paper rounded className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper rounded className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper rounded className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper rounded className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
 
-export default CenteredGrid;
+CenteredGrid.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CenteredGrid);
